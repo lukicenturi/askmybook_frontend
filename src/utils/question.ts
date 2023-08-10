7c1e163923d53ed641fa939fd7aaf81532b91528
lukicenturi <lukicenturi@gmail.com>
2023-08-10 13:41:19 +0700

    feat: add more random questions

--- src/utils/question.ts
+++ src/utils/question.ts
@@ -1,6 +1,12 @@
 const questions = [
+  'Who is the creator of this book, and when was it published?',
+  'What is this book about?',
   'What are the things that shown in balance sheet?',
-  'What are the disadvantages of a Partnership'
+  'What are the disadvantages of a Partnership?',
+  'Where can I get the information about my customers?',
+  'Why business planning is the most important factor determining business success?',
+  'What are the tips for for successful field research?',
+  'How to get the best loan deal?'
 ]
 
 export const getRandomQuestion = () => {
